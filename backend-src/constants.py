501bea4ca2f01fbee037eda84b24b3a1f5e710e6
Zachary Lam <lamzac26@gmail.com>
2024-10-17 18:55:41 +1100

    moved respective files to backend

---
+++ backend-src/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+API_URL = "https://graphql.csesoc.app/v1/graphql"
+
+UNIELECTIVES_URL = "https://unilectives.devsoc.app/course/"
+
+QUERY = """
+query MyQuery {
+  courses {
+    course_code
+    course_name
+    faculty
+    terms
+    uoc
+  }
+}
+"""
