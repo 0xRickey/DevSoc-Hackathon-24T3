7962da24def27e2e5b78f356631f93276e646ecc
Bianca Ren <z5417107@student.unsw.edu.au>
2024-10-17 14:54:36 +1100

    renamed frontend-src to src

--- frontend-src/getData/test.js
+++
@@ -1,6 +0,0 @@
-import { getSatisfiedPrereq , getAllCourses, getSatisfiedPrereqq } from './dataFetch.js';
-
-
-//console.log(getAllCourses())
-//console.log(getSatisfiedPrereq("Programming Languages", "2", ["COMP1511", "COMP2521"]))
-console.log(getSatisfiedPrereqq("Programming Languages", "2"))
