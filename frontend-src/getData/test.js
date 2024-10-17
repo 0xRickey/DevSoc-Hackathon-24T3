18445137e6c477062586e7dcb4b83e06f179daa9
Rickey <95059419+0xRickey@users.noreply.github.com>
2024-10-17 13:21:49 +1100

    Added frontend-src up to the point where terms are working

---
+++ frontend-src/getData/test.js
@@ -0,0 +1,6 @@
+import { getSatisfiedPrereq , getAllCourses, getSatisfiedPrereqq } from './dataFetch.js';
+
+
+//console.log(getAllCourses())
+//console.log(getSatisfiedPrereq("Programming Languages", "2", ["COMP1511", "COMP2521"]))
+console.log(getSatisfiedPrereqq("Programming Languages", "2"))
